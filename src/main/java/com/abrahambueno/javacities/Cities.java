195980d096cca98d922f167d2297d5d818dc9549
Abraham Bueno <abueno@localhost.localdomain>
2019-02-13 14:43:35 -0600

    Initial setup of database and interface

--- src/main/java/com/abrahambueno/javacities/Cities.java
+++ src/main/java/com/abrahambueno/javacities/Cities.java
@@ -1,4 +1,25 @@
 package com.abrahambueno.javacities;
 
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Data
+@Entity
 public class Cities {
+    private @Id @GeneratedValue Long id;
+    private String name;
+    private double homePrice;
+    private int affordableIndex;
+
+    public Cities() {
+    }
+
+    public Cities(String name, double homePrice, int affordableIndex) {
+        this.name = name;
+        this.homePrice = homePrice;
+        this.affordableIndex = affordableIndex;
+    }
 }
