4697db52ee23565017f5ad0bb5fd8f3596ac0c9b
Abraham Bueno <abueno@localhost.localdomain>
2019-02-13 15:34:23 -0600

    More setup complet, with the queue initialized to cities1, cities2, and messages

--- src/main/java/com/abrahambueno/javacities/CitiesMessageSender.java
+++ src/main/java/com/abrahambueno/javacities/CitiesMessageSender.java
@@ -1,4 +1,10 @@
 package com.abrahambueno.javacities;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
 public class CitiesMessageSender {
+
 }
