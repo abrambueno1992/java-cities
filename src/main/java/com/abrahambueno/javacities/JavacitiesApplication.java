31be057bf7213c821d6fa94ce647a378d1e2efb9
Abraham Bueno <abueno@localhost.localdomain>
2019-02-13 14:24:56 -0600

    Initial seupt

---
+++ src/main/java/com/abrahambueno/javacities/JavacitiesApplication.java
@@ -0,0 +1,14 @@
+package com.abrahambueno.javacities;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavacitiesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavacitiesApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/abrahambueno/javacities/JavacitiesApplicationTests.java
@@ -0,0 +1,17 @@
+package com.abrahambueno.javacities;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JavacitiesApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
