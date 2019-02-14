5fbb41561d369bc57e2c392fbdd1aef68a9db391
Abraham Bueno <abueno@localhost.localdomain>
2019-02-13 18:11:53 -0600

    Working on controller

--- src/main/java/com/abrahambueno/javacities/CitiesController.java
+++ src/main/java/com/abrahambueno/javacities/CitiesController.java
@@ -1,11 +1,26 @@
 package com.abrahambueno.javacities;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Slf4j
+@RequestMapping("/cities")
 public class CitiesController {
-//    private final CitiesRepository citiesrepos;
+    private final CitiesRepository citiesrepos;
+    private final RabbitTemplate ct;
 
+    public CitiesController(CitiesRepository citiesrepos, RabbitTemplate ct) {
+        this.citiesrepos = citiesrepos;
+        this.ct = ct;
+    }
+
+    @GetMapping("/afford")
+
+    @GetMapping("/homes")
+
+    @GetMapping("/name")
 }
